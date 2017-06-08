8efa88cadf1b3883e77e311a2c0da4699d1db549
TinaHeiligers <tina.heiligers@gmail.com>
2017-06-08 15:26:20 -0700

    stashing api/server/data/firebase-connector.js

--- api/server/data/firebase-connector.js
+++ api/server/data/firebase-connector.js
@@ -2,7 +2,7 @@
 
 const firebase = require('firebase-admin'),
 	// Follow the instructions in the README.md to get your own firebase credentials file, and point to that file here
-	creds = require('../../firebase_credentials.json');
+	creds = require('../../starwarscharacters-firebase-own.json');
 
 const db = firebase.initializeApp({
 	credential: firebase.credential.cert(creds.key),
